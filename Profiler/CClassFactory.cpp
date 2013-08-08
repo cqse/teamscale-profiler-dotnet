2a799a721847f4bad582fb3d8316cc702e3e9aa6
Martin Feilkas <feilkas@cqse.eu>
2013-08-08 08:07:18 +0000

    CR#5359: Renamed project folder

---
+++ Profiler/CClassFactory.cpp
@@ -0,0 +1,387 @@
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#include <windows.h>
+#include <stdio.h>
+#include "ProfilerCallback.h"
+
+
+HRESULT RegisterClassBase( REFCLSID rclsid,
+	const char *szDesc,                 
+	const char *szProgID,               
+	const char *szIndepProgID,          
+	char *szOutCLSID,
+	size_t nOutCLSIDLen );
+
+HRESULT UnregisterClassBase( REFCLSID rclsid,
+	const char *szProgID,
+	const char *szIndepProgID,
+	char *szOutCLSID,
+	size_t nOutCLSIDLen );
+
+BOOL SetRegValue( char *szKeyName, char *szKeyword, char *szValue );
+
+BOOL DeleteKey( const char *szKey, const char *szSubkey );
+
+BOOL SetKeyAndValue(const char *szKey,
+	const char *szSubkey,
+	const char *szValue );
+
+//=============================================================
+#define MAX_LENGTH 256
+#define PROFILER_GUID "{DD0A1BB6-11CE-11DD-8EE8-3F9E55D89593}"
+
+extern const GUID CLSID_PROFILER = 
+{ 0xDD0A1BB6, 0x11CE, 0x11DD, { 0x8E, 0xE8, 0x3F, 0x9E, 0x55, 0xD8, 0x95, 0x93 } };
+
+
+
+
+static const char *g_szProgIDPrefix = "Profiler";
+
+#define COM_METHOD( TYPE ) TYPE STDMETHODCALLTYPE
+
+HINSTANCE g_hInst;        // instance handle to this piece of code
+
+//==========================================================
+
+BOOL WINAPI DllMain( HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved )
+{    
+	// save off the instance handle for later use
+	switch ( dwReason )
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls( hInstance );
+		g_hInst = hInstance;
+		break;        
+	} 
+
+	return TRUE;
+}
+
+//================================================================
+
+class CClassFactory : public IClassFactory
+{
+public:
+	CClassFactory( )
+	{ 
+		m_refCount = 1; 
+	}
+
+	~CClassFactory( )
+	{
+		// nothing to do
+	}
+
+	COM_METHOD( ULONG ) AddRef()
+	{
+		return InterlockedIncrement( &m_refCount );
+	}
+	COM_METHOD( ULONG ) Release()
+	{
+		return InterlockedDecrement( &m_refCount );
+	}
+
+	COM_METHOD( HRESULT ) QueryInterface(REFIID riid, void **ppInterface );         
+
+	// IClassFactory methods
+	COM_METHOD( HRESULT ) LockServer( BOOL fLock ) { return S_OK; }
+	COM_METHOD( HRESULT ) CreateInstance( IUnknown *pUnkOuter,
+		REFIID riid,
+		void **ppInterface );
+
+private:
+
+	long m_refCount;                        
+};
+
+CClassFactory g_ProfilerClassFactory;
+
+//================================================================
+
+//#pragma comment(linker, "/EXPORT:DllUnregisterServer=_DllUnregisterServer@0,PRIVATE")
+STDAPI DllUnregisterServer()
+{    
+	char szID[128];         // the class ID to unregister.
+	char szCLSID[128];      // CLSID\\szID.
+	OLECHAR szWID[128];     // helper for the class ID to unregister.
+	char rcProgID[128];    // szProgIDPrefix.szClassProgID
+	char rcIndProgID[128]; // rcProgID.iVersion
+
+
+	// format the prog ID values.
+	sprintf_s( rcProgID,128, "%s.%s", g_szProgIDPrefix, PROFILER_GUID );
+	sprintf_s( rcIndProgID,128, "%s.%d", rcProgID, 1 );
+
+	memset (szCLSID, 0, sizeof(szCLSID));
+	UnregisterClassBase( CLSID_PROFILER, rcProgID, rcIndProgID, szCLSID, ARRAY_SIZE(szCLSID) );
+	DeleteKey( szCLSID, "InprocServer32" );
+
+	StringFromGUID2(CLSID_PROFILER, szWID, ARRAY_SIZE( szWID ) );
+	WideCharToMultiByte(CP_ACP, 0, szWID, -1, szID, sizeof(szID), NULL,NULL);
+
+	DeleteKey( "CLSID", szCLSID );
+
+	return S_OK;   
+}
+
+//================================================================
+
+//#pragma comment(linker, "/EXPORT:DllRegisterServer=_DllRegisterServer@0,PRIVATE")
+STDAPI DllRegisterServer()
+{    
+	HRESULT hr = S_OK;
+	char  szModule[_MAX_PATH];  
+
+	DllUnregisterServer();
+	GetModuleFileNameA( g_hInst, szModule, ARRAY_SIZE(szModule) );
+
+	char rcCLSID[MAX_LENGTH];           // CLSID\\szID.
+	char rcProgID[MAX_LENGTH];          // szProgIDPrefix.szClassProgID
+	char rcIndProgID[MAX_LENGTH];       // rcProgID.iVersion
+	char rcInproc[MAX_LENGTH + 2];      // CLSID\\InprocServer32
+
+
+	// format the prog ID values.
+	sprintf_s( rcIndProgID,MAX_LENGTH, "%s.%s", g_szProgIDPrefix, PROFILER_GUID ) ;
+	sprintf_s( rcProgID,MAX_LENGTH, "%s.%d", rcIndProgID, 1 );
+
+	// do the initial portion.
+	hr =  RegisterClassBase( CLSID_PROFILER, 
+		"Profiler", rcProgID, rcIndProgID, rcCLSID,
+		ARRAY_SIZE (rcCLSID));
+
+	if ( SUCCEEDED( hr ) )
+	{
+		// set the server path.
+		SetKeyAndValue( rcCLSID, "InprocServer32", szModule );
+
+		// add the threading model information.
+		sprintf_s( rcInproc,MAX_LENGTH+2, "%s\\%s", rcCLSID, "InprocServer32" );
+		SetRegValue( rcInproc, "ThreadingModel", "Both" );
+	}   
+	else
+		DllUnregisterServer();
+
+	return hr;    
+}
+
+//================================================================
+
+//#pragma comment(linker, "/EXPORT:DllGetClassObject=_DllGetClassObject@12,PRIVATE")
+STDAPI DllGetClassObject( REFCLSID rclsid, REFIID riid, LPVOID FAR *ppv )                  
+{    
+	HRESULT hr = E_OUTOFMEMORY;
+
+	if ( rclsid == CLSID_PROFILER )
+		hr = g_ProfilerClassFactory.QueryInterface( riid, ppv );
+
+	return hr;   
+}
+
+//===========================================================
+
+HRESULT CClassFactory::QueryInterface( REFIID riid, void **ppInterface )
+{    
+	if ( riid == IID_IUnknown )
+		*ppInterface = static_cast<IUnknown *>( this ); 
+	else if ( riid == IID_IClassFactory )
+		*ppInterface = static_cast<IClassFactory *>( this );
+	else
+	{
+		*ppInterface = NULL;                                  
+		return E_NOINTERFACE;
+	}
+
+	reinterpret_cast<IUnknown *>( *ppInterface )->AddRef();
+
+	return S_OK;
+}
+
+// Used to determine whether the DLL can be unloaded by COM
+//#pragma comment(linker, "/EXPORT:DllCanUnloadNow=_DllCanUnloadNow@0,PRIVATE")
+STDAPI DllCanUnloadNow(void)
+{
+	return S_OK;
+}
+
+//===========================================================
+
+HRESULT CClassFactory::CreateInstance( IUnknown *pUnkOuter, REFIID riid,
+	void **ppInstance )
+{       
+	// aggregation is not supported by these objects
+	if ( pUnkOuter != NULL )
+		return CLASS_E_NOAGGREGATION;
+
+	CProfilerCallback * pProfilerCallback = new CProfilerCallback();
+
+	*ppInstance = (void *)pProfilerCallback;
+
+	return S_OK;
+}
+
+//===========================================================
+
+HRESULT RegisterClassBase( REFCLSID rclsid,
+	const char *szDesc,                 
+	const char *szProgID,               
+	const char *szIndepProgID,          
+	char *szOutCLSID,
+	size_t nOutCLSIDLen)              
+{
+	char szID[64];     // the class ID to register.
+	OLECHAR szWID[64]; // helper for the class ID to register.
+
+	StringFromGUID2( rclsid, szWID, ARRAY_SIZE( szWID ) );
+	WideCharToMultiByte(CP_ACP, 0, szWID, -1, szID, sizeof(szID), NULL, NULL);
+
+	strcpy_s( szOutCLSID, nOutCLSIDLen, "CLSID\\" );
+	strcat_s( szOutCLSID, nOutCLSIDLen, szID );
+
+	BOOL success = TRUE;
+
+	// create ProgID keys.
+	success &=SetKeyAndValue( szProgID, NULL, szDesc );
+	success &=SetKeyAndValue( szProgID, "CLSID", szID );
+
+	// create VersionIndependentProgID keys.
+	success &=SetKeyAndValue( szIndepProgID, NULL, szDesc );
+	success &= SetKeyAndValue( szIndepProgID, "CurVer", szProgID );
+	success &=SetKeyAndValue( szIndepProgID, "CLSID", szID );
+
+	// create entries under CLSID.
+	success &=SetKeyAndValue( szOutCLSID, NULL, szDesc );
+	success &=SetKeyAndValue( szOutCLSID, "ProgID", szProgID );
+	success &=SetKeyAndValue( szOutCLSID, "VersionIndependentProgID", szIndepProgID );
+	success &=SetKeyAndValue( szOutCLSID, "NotInsertable", NULL );
+
+	if(success) return S_OK;
+	else return S_FALSE;
+}
+
+//===========================================================
+
+HRESULT UnregisterClassBase( REFCLSID rclsid,
+	const char *szProgID,
+	const char *szIndepProgID,
+	char *szOutCLSID,
+	size_t nOutCLSIDLen )
+{
+	char szID[64];     // the class ID to register.
+	OLECHAR szWID[64]; // helper for the class ID to register.
+
+	StringFromGUID2( rclsid, szWID, ARRAY_SIZE( szWID ) );
+	WideCharToMultiByte(CP_ACP, 0, szWID, -1, szID, sizeof(szID), NULL, NULL);
+
+	strcpy_s( szOutCLSID, nOutCLSIDLen, "CLSID\\" );
+	strcat_s( szOutCLSID, nOutCLSIDLen, szID );
+
+	// delete the version independant prog ID settings.
+	DeleteKey( szIndepProgID, "CurVer" );
+	DeleteKey( szIndepProgID, "CLSID" );
+	RegDeleteKeyA( HKEY_CLASSES_ROOT, szIndepProgID );
+
+	// delete the prog ID settings.
+	DeleteKey( szProgID, "CLSID" );
+	RegDeleteKeyA( HKEY_CLASSES_ROOT, szProgID );
+
+	// delete the class ID settings.
+	DeleteKey( szOutCLSID, "ProgID" );
+	DeleteKey( szOutCLSID, "VersionIndependentProgID" );
+	DeleteKey( szOutCLSID, "NotInsertable" );
+	RegDeleteKeyA( HKEY_CLASSES_ROOT, szOutCLSID );
+
+	return S_OK;
+}
+
+//===========================================================
+
+BOOL DeleteKey( const char *szKey,
+	const char *szSubkey )
+{
+	char rcKey[MAX_LENGTH]; // buffer for the full key name.
+
+	sprintf_s (rcKey, ARRAY_SIZE(rcKey), "%s\\%s", szKey, szSubkey);
+
+	char buf[256];
+	sprintf_s (buf, ARRAY_SIZE(buf), "Length=%d", strlen (rcKey));
+
+	// delete the registration key.
+	RegDeleteKeyA( HKEY_CLASSES_ROOT, rcKey );
+
+	return TRUE;
+}
+
+//===========================================================
+
+BOOL SetKeyAndValue( const char *szKey,
+	const char *szSubkey,
+	const char *szValue )
+{
+	HKEY hKey;              // handle to the new reg key.
+	char rcKey[MAX_LENGTH]; // buffer for the full key name.
+
+	// init the key with the base key name.
+	strcpy_s( rcKey,MAX_LENGTH, szKey );
+
+	// append the subkey name (if there is one).
+	if ( szSubkey != NULL )
+	{
+		strcat_s( rcKey,MAX_LENGTH, "\\" );
+		strcat_s( rcKey,MAX_LENGTH, szSubkey );
+	}
+
+	// create the registration key.
+	long ec = RegCreateKeyExA( HKEY_CLASSES_ROOT, 
+		rcKey, 
+		0, 
+		NULL,
+		REG_OPTION_NON_VOLATILE, 
+		KEY_ALL_ACCESS, 
+		NULL,
+		&hKey, 
+		NULL );
+	if (ec  == ERROR_SUCCESS )
+	{
+		// set the value (if there is one).
+		if ( szValue != NULL )
+		{
+			RegSetValueExA( hKey, NULL, 0, REG_SZ, (BYTE *)szValue,
+				(DWORD)(((strlen(szValue) + 1) * sizeof (char))));
+		}
+
+		RegCloseKey( hKey );
+
+		return TRUE;
+	}
+
+	return FALSE;   
+}
+
+BOOL SetRegValue(char *szKeyName, char *szKeyword, char *szValue)
+{
+	HKEY hKey; // handle to the new reg key.
+
+	// create the registration key.
+	if ( RegCreateKeyExA( HKEY_CLASSES_ROOT, 
+		szKeyName, 0,  NULL,
+		REG_OPTION_NON_VOLATILE, 
+		KEY_ALL_ACCESS, 
+		NULL, &hKey, 
+		NULL) == ERROR_SUCCESS )
+	{
+		// set the value (if there is one).
+		if ( szValue != NULL )
+		{
+			RegSetValueExA( hKey, szKeyword, 0, REG_SZ, 
+				(BYTE *)szValue, 
+				(DWORD)((strlen(szValue) + 1) * sizeof ( char )));
+		}
+
+		RegCloseKey( hKey );
+
+		return TRUE;
+	}
+
+	return FALSE;
+}
---
+++ Profiler/CProfilerCallbackBase.cpp
@@ -0,0 +1,471 @@
+#include "CProfilerCallbackBase.h"
+
+
+
+/** Constructor */
+CProfilerCallbackBase::CProfilerCallbackBase() : m_ref_cnt(0){
+	// do nothing but inialization performed in m_ref_cnt
+}
+
+/** Destructor */
+CProfilerCallbackBase::~CProfilerCallbackBase() {
+	// do nothing 
+}
+
+//====================================================
+// IUnknown implementation
+//====================================================
+ULONG CProfilerCallbackBase::AddRef()
+{
+	return InterlockedIncrement(&m_ref_cnt);
+}
+
+ULONG CProfilerCallbackBase::Release()
+{
+	return InterlockedDecrement(&m_ref_cnt);
+}
+
+HRESULT CProfilerCallbackBase::QueryInterface( REFIID riid, void **ppInterface )
+{
+	if(riid == IID_IUnknown)
+	{
+		*ppInterface = static_cast<ICorProfilerCallback*>(this);
+		return S_OK;
+	}
+	else if(riid == IID_ICorProfilerCallback)
+	{
+		*ppInterface = static_cast<ICorProfilerCallback*>(this);
+		return S_OK;
+	}
+	else if(riid == IID_ICorProfilerCallback2)
+	{
+		*ppInterface = static_cast<ICorProfilerCallback2*>(this);
+		return S_OK;
+	}
+    else if(riid == IID_ICorProfilerCallback3)
+	{
+		*ppInterface = static_cast<ICorProfilerCallback3*>(this);
+		return S_OK;
+	}
+    return E_NOTIMPL;
+}
+//====================================================
+// End IUnknown implementation
+//====================================================
+
+
+
+//====================================================
+// Profiling interface implementation
+// 
+// Provides default implementations for all methods. 
+// Implementations are contained in the subclass.
+// This allows for clean separation of default implementations 
+// of callbacks that are not required for coverage profiling, 
+// and code specific for the coverage profiler.
+//
+//====================================================
+STDMETHODIMP CProfilerCallbackBase::Initialize(IUnknown *pICorProfilerInfoUnk)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::Shutdown()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::AppDomainCreationStarted(AppDomainID appDomainID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::AppDomainCreationFinished(AppDomainID appDomainID, HRESULT hrStatus)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::AppDomainShutdownStarted(AppDomainID appDomainID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::AppDomainShutdownFinished(AppDomainID appDomainID, HRESULT hrStatus)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::AssemblyLoadStarted(AssemblyID assemblyID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::AssemblyLoadFinished(AssemblyID assemblyID, HRESULT hrStatus)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::AssemblyUnloadStarted(AssemblyID assemblyID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::AssemblyUnloadFinished(AssemblyID assemblyID, HRESULT hrStatus)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ModuleLoadStarted(ModuleID moduleID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ModuleLoadFinished(ModuleID moduleID, HRESULT hrStatus)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ModuleUnloadStarted(ModuleID moduleID)
+{
+    return S_OK;
+}
+	  
+STDMETHODIMP CProfilerCallbackBase::ModuleUnloadFinished(ModuleID moduleID, HRESULT hrStatus)
+{
+	return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ModuleAttachedToAssembly(ModuleID moduleID, AssemblyID assemblyID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ClassLoadStarted(ClassID classID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ClassLoadFinished(ClassID classID, HRESULT hrStatus)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ClassUnloadStarted(ClassID classID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ClassUnloadFinished(ClassID classID, HRESULT hrStatus)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::FunctionUnloadStarted(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::JITCompilationStarted(FunctionID functionID, BOOL fIsSafeToBlock)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::JITCompilationFinished(FunctionID functionID, HRESULT hrStatus, BOOL fIsSafeToBlock)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::JITCachedFunctionSearchStarted(FunctionID functionID, BOOL *pbUseCachedFunction)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::JITCachedFunctionSearchFinished(FunctionID functionID, COR_PRF_JIT_CACHE result)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::JITFunctionPitched(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::JITInlining(FunctionID callerID, FunctionID calleeID, BOOL *pfShouldInline)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::UnmanagedToManagedTransition(FunctionID functionID, COR_PRF_TRANSITION_REASON reason)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ManagedToUnmanagedTransition(FunctionID functionID, COR_PRF_TRANSITION_REASON reason)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ThreadCreated(ThreadID threadID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ThreadDestroyed(ThreadID threadID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ThreadAssignedToOSThread(ThreadID managedThreadID, DWORD osThreadID) 
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RemotingClientInvocationStarted()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RemotingClientSendingMessage(GUID *pCookie, BOOL fIsAsync)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RemotingClientReceivingReply(GUID *pCookie, BOOL fIsAsync)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RemotingClientInvocationFinished()
+{
+	return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RemotingServerReceivingMessage(GUID *pCookie, BOOL fIsAsync)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RemotingServerInvocationStarted()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RemotingServerInvocationReturned()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RemotingServerSendingReply(GUID *pCookie, BOOL fIsAsync)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RuntimeSuspendStarted(COR_PRF_SUSPEND_REASON suspendReason)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RuntimeSuspendFinished()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RuntimeSuspendAborted()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RuntimeResumeStarted()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RuntimeResumeFinished()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RuntimeThreadSuspended(ThreadID threadID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RuntimeThreadResumed(ThreadID threadID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::MovedReferences(ULONG cmovedObjectIDRanges, ObjectID oldObjectIDRangeStart[], ObjectID newObjectIDRangeStart[], ULONG cObjectIDRangeLength[])
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ObjectAllocated(ObjectID objectID, ClassID classID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ObjectsAllocatedByClass(ULONG classCount, ClassID classIDs[], ULONG objects[])
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ObjectReferences(ObjectID objectID, ClassID classID, ULONG objectRefs, ObjectID objectRefIDs[])
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RootReferences(ULONG rootRefs, ObjectID rootRefIDs[])
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionThrown(ObjectID thrownObjectID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionUnwindFunctionEnter(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionUnwindFunctionLeave()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionSearchFunctionEnter(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionSearchFunctionLeave()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionSearchFilterEnter(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionSearchFilterLeave()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionSearchCatcherFound(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionCLRCatcherFound()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionCLRCatcherExecute()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionOSHandlerEnter(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionOSHandlerLeave(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionUnwindFinallyEnter(FunctionID functionID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionUnwindFinallyLeave()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionCatcherEnter(FunctionID functionID,
+    											 ObjectID objectID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ExceptionCatcherLeave()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::COMClassicVTableCreated(ClassID wrappedClassID, REFGUID implementedIID, void *pVTable, ULONG cSlots)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::COMClassicVTableDestroyed(ClassID wrappedClassID, REFGUID implementedIID, void *pVTable)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ThreadNameChanged(ThreadID threadID, ULONG cchName, WCHAR name[])
+{
+	return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::GarbageCollectionStarted(int cGenerations, BOOL generationCollected[], COR_PRF_GC_REASON reason)
+{
+	return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::SurvivingReferences(ULONG cSurvivingObjectIDRanges, ObjectID objectIDRangeStart[], ULONG cObjectIDRangeLength[])
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::GarbageCollectionFinished()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::FinalizeableObjectQueued(DWORD finalizerFlags, ObjectID objectID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::RootReferences2(ULONG cRootRefs, ObjectID rootRefIDs[], COR_PRF_GC_ROOT_KIND rootKinds[], COR_PRF_GC_ROOT_FLAGS rootFlags[], UINT_PTR rootIDs[])
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::HandleCreated(GCHandleID handleID, ObjectID initialObjectID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::HandleDestroyed(GCHandleID handleID)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::InitializeForAttach(IUnknown * pCorProfilerInfoUnk,void * pvClientData, UINT cbClientData)
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ProfilerAttachComplete()
+{
+    return S_OK;
+}
+
+STDMETHODIMP CProfilerCallbackBase::ProfilerDetachSucceeded()
+{
+    return S_OK;
+}
+
+//====================================================
+// End of Profiling interface implementation
+//====================================================
