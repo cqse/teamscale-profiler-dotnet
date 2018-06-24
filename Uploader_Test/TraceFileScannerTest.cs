73d04f6330aa42a3539f81bec50d735a5ff18d6b
Fabian Streitel <streitel@cqse.eu>
2018-06-24 22:48:45 +0200

    test exception handling during directory scan

--- Uploader_Test/TraceFileScannerTest.cs
+++ Uploader_Test/TraceFileScannerTest.cs
@@ -5,6 +5,7 @@ using System.IO.Abstractions;
 using System.IO.Abstractions.TestingHelpers;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 [TestClass]
 public class TraceFileScannerTest
@@ -13,7 +14,7 @@ public class TraceFileScannerTest
     private const string VERSION_ASSEMBLY = "VersionAssembly";
 
     [TestMethod]
-    public void TestAllScenarios()
+    public void TestAllFileContents()
     {
         IFileSystem fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>()
         {
@@ -51,12 +52,64 @@ Inlined=1:33555646:100678050
         });
     }
 
+    [TestMethod]
+    public void ExceptionsShouldLeadToFileBeingIgnored()
+    {
+        Mock<IFileSystem> fileSystemMock = new Mock<IFileSystem>();
+        Mock<FileBase> fileMock = new Mock<FileBase>();
+        Mock<DirectoryBase> directoryMock = new Mock<DirectoryBase>();
+
+        fileMock.Setup(file => file.ReadAllLines("coverage_1_1.txt")).Throws<IOException>();
+        fileMock.Setup(file => file.ReadAllLines("coverage_1_2.txt")).Returns(new string[] {
+            "Assembly=VersionAssembly:1 Version:4.0.0.0",
+            "Inlined=1:33555646:100678050",
+        });
+        directoryMock.Setup(directory => directory.EnumerateFiles(It.IsAny<string>()))
+            .Returns(new string[] { "coverage_1_1.txt", "coverage_1_2.txt" });
+
+        fileSystemMock.Setup(fileSystem => fileSystem.File).Returns(fileMock.Object);
+        fileSystemMock.Setup(fileSystem => fileSystem.Directory).Returns(directoryMock.Object);
+
+        IEnumerable<TraceFileScanner.ScannedFile> files =
+            new TraceFileScanner(TRACE_DIRECTORY, VERSION_ASSEMBLY, fileSystemMock.Object).ListTraceFilesReadyForUpload();
+
+        files.Should().HaveCount(1).And.Contain(new TraceFileScanner.ScannedFile[]
+        {
+            new TraceFileScanner.ScannedFile()
+            {
+                FilePath = "coverage_1_2.txt",
+                Version = "4.0.0.0",
+            },
+        });
+    }
+
     /// <summary>
     /// Returns a file with the given name in the trace directory.
     /// </summary>
     private string File(string fileName)
     {
         return Path.Combine(TRACE_DIRECTORY, fileName);
+    }
+
+    public class ThrowingMockFileData : MockFileData
+    {
+        public ThrowingMockFileData() : base("")
+        {
+            // nothing to do
+        }
+
+        new byte[] Contents
+        {
+            get
+            {
+                throw new IOException("IO failed");
+            }
+
+            set
+            {
+                throw new IOException("IO failed");
+            }
+        }
     }
 }
 
