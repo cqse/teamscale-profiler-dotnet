b1a716957a2fd54c6d9c7c0d138c7b81e6ea3fbe
Sven Amann <amann@cqse.eu>
2018-10-12 14:18:36 +0200

    Test application-type heuristic.

---
+++ ProfilerGUI_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProfilerGUI_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProfilerGUI_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("07c6f87e-27fa-4027-ae11-94ae58b1bff4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProfilerGUI_Test/TargetAppModelTest.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using ProfilerGUI.Source.Configurator;
+using ProfilerGUI.Source.Shared;
+using System.IO;
+
+namespace Cqse.Teamscale.Profiler.Dotnet
+{
+    [TestFixture]
+    public class TargetAppModelTest
+    {
+        [Test]
+        public void TestDetectDotNETCoreApplicationType()
+        {
+            TargetAppModel targetAppModel = CreateModelForTestApplication("DotNetCoreApp.dll");
+
+            Assert.AreEqual(EApplicationType.TypeCORE, targetAppModel.ApplicationType);
+        }
+
+        [Test]
+        public void TestDetectDotNETFramework32BitApplicationType()
+        {
+            TargetAppModel targetAppModel = CreateModelForTestApplication("DotNetFW32BitApp.exe");
+
+            Assert.AreEqual(EApplicationType.Type32Bit, targetAppModel.ApplicationType);
+        }
+
+        [Test]
+        public void TestDetectDotNETFramework64BitApplicationType()
+        {
+            TargetAppModel targetAppModel = CreateModelForTestApplication("DotNetFW64BitApp.exe");
+
+            Assert.AreEqual(EApplicationType.Type64Bit, targetAppModel.ApplicationType);
+        }
+
+        [Test]
+        public void TestDetectNonDotNet32BitApplicationType()
+        {
+            TargetAppModel targetAppModel = CreateModelForTestApplication("NonDotNet32BitApp.exe");
+
+            Assert.AreEqual(EApplicationType.Type32Bit, targetAppModel.ApplicationType);
+        }
+
+        [Test]
+        public void TestDetectNonDotNet64BitApplicationType()
+        {
+            TargetAppModel targetAppModel = CreateModelForTestApplication("NonDotNet64BitApp.exe");
+
+            Assert.AreEqual(EApplicationType.Type64Bit, targetAppModel.ApplicationType);
+        }
+
+        private static TargetAppModel CreateModelForTestApplication(string application)
+        {
+            var targetAppModel = new TargetAppModel(new ProfilerConfiguration());
+            targetAppModel.ApplicationPath = GetTestDataPath("test-programs", application);
+            return targetAppModel;
+        }
+
+        /// <summary>
+        /// The directory containing profiler solution.
+        /// </summary>
+        public static DirectoryInfo SolutionRoot =>
+            new DirectoryInfo(Path.Combine(TestContext.CurrentContext.TestDirectory, "../../.."));
+
+        /// <summary>
+		/// Returns the absolute path to a test data file.
+		/// </summary>
+		protected static string GetTestDataPath(params string[] path)
+            => Path.Combine(SolutionRoot.FullName, "test-data", Path.Combine(path));
+    }
+}
