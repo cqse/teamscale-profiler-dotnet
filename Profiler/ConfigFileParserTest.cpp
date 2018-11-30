0f70036eac274fc97d1d34c510c1a33c33243b16
Fabian Streitel <streitel@cqse.eu>
2018-11-30 11:59:50 +0100

    add additional tests

--- Profiler/ConfigFileParserTest.cpp
+++ Profiler/ConfigFileParserTest.cpp
@@ -11,7 +11,7 @@ public:
 	TEST_METHOD(ValidEmptyConfig)
 	{
 		ConfigFile file = parse(R"()");
-		Assert::IsTrue(file.sections.empty());
+		Assert::IsTrue(file.sections.empty(), L"expecting no sections");
 	}
 
 	TEST_METHOD(InvalidYamlFile)
@@ -28,7 +28,51 @@ match:
   "foo.*":
     enabled: "0"
 )");
-		Assert::AreEqual(1, (int)file.sections.size());
+		Assert::AreEqual(1, (int)file.sections.size(), L"number of sections");
+		Assert::IsTrue(std::regex_match("foobar", file.sections[0].processRegex), L"expecting regex to match");
+		Assert::AreEqual("0", file.sections[0].options["enabled"].c_str(), L"enabled option");
+	}
+
+	TEST_METHOD(ConfigOptionsCaseInsensitive)
+	{
+		ConfigFile file = parse(R"(
+match:
+  "foo.*":
+    ENablED: "1"
+)");
+		Assert::AreEqual(1, (int)file.sections.size(), L"number of sections");
+		Assert::AreEqual("1", file.sections[0].options["enabled"].c_str(), L"enabled option");
+	}
+
+	TEST_METHOD(SectionOrderPreserved)
+	{
+		ConfigFile file = parse(R"(
+match:
+  foo:
+  zzz:
+  "bar":
+  "__123":
+  zzz:
+)");
+		Assert::AreEqual(5, (int)file.sections.size(), L"number of sections");
+		Assert::IsTrue(std::regex_match("foo", file.sections[0].processRegex), L"expecting section 0 match");
+		Assert::IsTrue(std::regex_match("zzz", file.sections[1].processRegex), L"expecting section 1 match");
+		Assert::IsTrue(std::regex_match("bar", file.sections[2].processRegex), L"expecting section 2 match");
+		Assert::IsTrue(std::regex_match("__123", file.sections[3].processRegex), L"expecting section 3 match");
+		Assert::IsTrue(std::regex_match("__123", file.sections[3].processRegex), L"expecting section 4 match");
+	}
+
+	TEST_METHOD(TypeConversion)
+	{
+		ConfigFile file = parse(R"(
+match:
+  "foo.*":
+    enabled: true
+    enabled2: 1
+)");
+		Assert::AreEqual(1, (int)file.sections.size(), L"number of sections");
+		Assert::AreEqual("true", file.sections[0].options["enabled"].c_str(), L"enabled option");
+		Assert::AreEqual("1", file.sections[0].options["enabled2"].c_str(), L"enabled option");
 	}
 
 private:
