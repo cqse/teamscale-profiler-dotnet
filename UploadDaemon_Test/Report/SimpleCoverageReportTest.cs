abd3188ab58c15103361a914130d8559cc6d42d9
Sven Amann <amann@cqse.eu>
2021-01-31 21:12:22 +0100

    Tests simple report output.

---
+++ UploadDaemon_Test/Report/SimpleCoverageReportTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UploadDaemon.Report;
+using UploadDaemon.SymbolAnalysis;
+
+namespace UploadDaemons.Report
+{
+    [TestFixture]
+    public class SimpleCoverageReportTest
+    {
+        [Test]
+        public void ConvertsToReportFormat()
+        {
+            SimpleCoverageReport report = new SimpleCoverageReport(new LineCoverageReport(new Dictionary<string, FileCoverage>() {
+                { "file1.cs", new FileCoverage((1,5),(7,10))},
+                { "file2.cs", new FileCoverage((3, 20)) }
+            }));
+
+            Assert.That(report.ToString(), Is.EqualTo(@"# isMethodAccurate=true
+file1.cs
+1-5
+7-10
+file2.cs
+3-20
+"));
+        }
+    }
+}
