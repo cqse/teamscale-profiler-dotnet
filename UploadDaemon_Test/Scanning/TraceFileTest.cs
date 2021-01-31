c379efad68554d4a2821e34ef4f625f495692c7a
Sven Amann <amann@cqse.eu>
2021-01-31 10:26:40 +0100

    Ignores method references from unkown assemblies.

--- UploadDaemon_Test/Scanning/TraceFileTest.cs
+++ UploadDaemon_Test/Scanning/TraceFileTest.cs
@@ -15,7 +15,7 @@ namespace UploadDaemon.Scanning
             $"Inlined=2:12345",
             });
 
-            Assert.That(trace.FindCoveredMethods, Has.Count.EqualTo(1));
+            Assert.That(trace.FindCoveredMethods(), Has.Count.EqualTo(1));
             Assert.That(trace.FindCoveredMethods()[0].Item1, Is.EqualTo("ProfilerGUI"));
             Assert.That(trace.FindCoveredMethods()[0].Item2, Is.EqualTo(12345));
         }
@@ -29,9 +29,20 @@ namespace UploadDaemon.Scanning
             $"Inlined=2:9876:12345",
             });
 
-            Assert.That(trace.FindCoveredMethods, Has.Count.EqualTo(1));
+            Assert.That(trace.FindCoveredMethods(), Has.Count.EqualTo(1));
             Assert.That(trace.FindCoveredMethods()[0].Item1, Is.EqualTo("ProfilerGUI"));
             Assert.That(trace.FindCoveredMethods()[0].Item2, Is.EqualTo(12345));
+        }
+
+        [Test]
+        public void IgnoresMethodReferenceFromUnknownAssembly()
+        {
+            TraceFile trace = new TraceFile(":path:", new string[]
+            {
+            $"Inlined=1:12345",
+            });
+
+            Assert.That(trace.FindCoveredMethods(), Is.Empty);
         }
     }
 }
