ef2410a4d8a78b889d8224a35a3380d05f1b2607
Martin Pöhlmann <poehlmann@cqse.eu>
2022-03-24 16:31:35 +0100

    Commander Server

---
+++ Commander.Server/ProfilerTestController.cs
@@ -0,0 +1,41 @@
+﻿using Cqse.Teamscale.Profiler.Commons.Ipc;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace Cqse.Teamscale.Profiler.Commander.Server
+{
+    [Route("test")]
+    [ApiController]
+    public class ProfilerTestController : ControllerBase
+    {
+        private readonly ProfilerIpc profilerIpc;
+
+        public ProfilerTestController(ProfilerIpc profilerIpc)
+        {
+            this.profilerIpc = profilerIpc;
+        }
+
+        [HttpGet]
+        public string GetCurrent()
+        {
+            return profilerIpc.TestName;
+        }
+
+        [HttpPost("start/{testName}")]
+        public void startTest(string testName)
+        {
+            if (string.IsNullOrEmpty(testName))
+            {
+                throw new BadHttpRequestException("Test name may not be empty");
+            }
+
+            profilerIpc.StartTest(HttpUtility.UrlDecode(testName));
+        }
+
+        [HttpPost("stop/{result}")]
+        public void stopTest(TestExecutionResult result)
+        {
+            profilerIpc.EndTest(result);
+        }
+    }
+}
