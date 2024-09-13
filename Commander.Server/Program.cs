c9943064cb83543aad5f4e31faef486d966cf2c7
snowbagoly <leona.thet@gmail.com>
2024-09-13 16:18:47 +0200

    TS#38029 Delete Commander.CLI and wwwroot from Commander.Server

--- Commander.Server/Program.cs
+++ Commander.Server/Program.cs
@@ -1,6 +1,6 @@
 using Cqse.Teamscale.Profiler.Commons.Ipc;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions());
 // explicitly initialize the Ipc, otherwise it might be created upon 1st request and we miss the start event in the profiler
 builder.Services.AddSingleton<ProfilerIpc>(new ProfilerIpc(new IpcConfig()));
 builder.Services.AddControllers();
